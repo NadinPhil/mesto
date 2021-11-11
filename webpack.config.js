a279caa4e12625278b2a249954612ff98d42d3c0
Nadya Filimonova <filim0nova.nadezhda@yandex.ru>
2021-11-11 22:28:24 +0500

    ПР 8 Сделать Веб пак 2

--- webpack.config.js
+++ webpack.config.js
@@ -21,25 +21,22 @@ module.exports = {
   module: {
     rules: [ 
       {
-        
         test: /\.js$/,
         use: 'babel-loader',
         exclude: '/node_modules/'
       },
       
       {
-      
       test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
       type: 'asset/resource'
       },
       {
-        
         test: /\.css$/,
-        
-        use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader',
-          options: { importLoaders: 1 },
-        },
+        use: [MiniCssExtractPlugin.loader, 
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1 },
+          },
         'postcss-loader']
       },
     ]

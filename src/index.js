8a17201f8278434d641988f166f1e34dd792e87c
Nadya Filimonova <filim0nova.nadezhda@yandex.ru>
2021-11-15 19:51:42 +0500

    ПР № 8 На проверку 3

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import Section from "../components/Section.js";
 import Popup from "../components/Popup.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import PopupWithImage from "../components/PopupWithImage.js";
-import '../pages/index.css'; 
-
+import '../pages/index.css';
 
 const popupEdit = document.querySelector('.popup_type_edit');
 const openPopupEditButton = document.querySelector('.profile__edit');
